690bf70ceed78a73fa7e7f107483b602783b539c
Leonardo Ralph <theleoralph@gmail.com>
2023-06-05 14:35:22 -0300

    Add relationships

--- database/migrations/2023_06_05_171559_create_expenses_table.php
+++ database/migrations/2023_06_05_171559_create_expenses_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,6 +14,10 @@ return new class extends Migration
     {
         Schema::create('expenses', function (Blueprint $table) {
             $table->id();
+            $table->foreignIdFor(User::class)->constrained();
+            $table->string('description', 191);
+            $table->decimal('value', 8, 2);
+            $table->date('date');
             $table->timestamps();
         });
     }
