347cb3e25d11999fa0fd3bcbd4c86c3875b3e06b
Leonardo Ralph <theleoralph@gmail.com>
2023-06-08 23:13:52 -0300

    Add lang files

---
+++ lang/en/messages.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'expense' => [
+        'created' => 'Expense created successfully.',
+        'updated' => 'Expense updated successfully.',
+        'deleted' => 'Expense deleted successfully.'
+    ],
+    'user' => [
+        'created' => 'User created successfully.',
+        'updated' => 'User updated successfully.'
+    ]
+];
---
+++ lang/pt_BR/messages.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    'expense' => [
+        'created' => 'Despesa criada com sucesso.',
+        'updated' => 'Despesa atualizada com sucesso.',
+        'deleted' => 'Despesa excluída com sucesso.'
+    ],
+    'auth' => [
+        'failed' => 'Credenciais inválidas.',
+        'success' => 'Login realizado com sucesso.',
+        'logout' => 'Logout realizado com sucesso.',
+        'register' => 'Usuário cadastrado com sucesso.'
+    ],
+    'user' => [
+        'created' => 'Usuário criado com sucesso.',
+        'updated' => 'Usuário atualizado com sucesso.'
+    ]
+];
